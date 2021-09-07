d73102d98166a02b5453a10267e1c84bec76f31c
Ana Lúcia <ana.medeiros0097@gmail.com>
2021-09-07 10:13:37 -0300

    Segundo commit

--- src/graphql/modules/posts/resolvers.js
+++ src/graphql/modules/posts/resolvers.js
@@ -1,9 +1,4 @@
 import Post from '../../../models/postModel';
-// Os resolvers são funções que vão lidar com as rotas.
-// Por exemplo, vamos dizer que queremos pegar todos os usuários que temos no nosso banco de dados.
-// Então vamos criar uma rota em Query que pede essa lista de todos os usuários. 
-// E ai, em resolvers, é que a gente definir uma função que vai buscar essa lista de usuários no banco de dados.
-// São os resolvers que vão fazer as ações definidas pelo meu schema.
 
 const resolvers = {
     Query: {
